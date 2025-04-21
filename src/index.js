1f546cf442f4957ff79903bd49e470541cb254a2
Daniel S. Davis <daniel.davis@fullsteam.com>
2025-04-20 21:22:52 -0400

    Updated delay test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const core = require("@actions/core");
 const axios = require('axios');
 const delay = require('./delay');
 
+async function test() {
+    console.log("test");
+    await delay(5000);
+    console.log("another");
+}
+
 try {
     const tenant_id = core.getInput('tenant_id');
     const token = core.getInput('token');
@@ -9,9 +15,7 @@ try {
     const client_secret = core.getInput('client_secret');
 
     if(token == undefined) {
-        console.log("test");
-        await delay(5000);
-        console.log("another");
+        test();
     }
 
 } catch (error) {
